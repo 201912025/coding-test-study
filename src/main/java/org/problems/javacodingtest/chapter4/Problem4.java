d33cfc5afe58d9d2f56bf922956fb469b0269cb2
201912025 <michi2012@naver.com>
2025-08-13 14:48:32 +0900

    chapter4 : Problem4

--- src/main/java/org/problems/javacodingtest/chapter4/Problem4.java
+++ src/main/java/org/problems/javacodingtest/chapter4/Problem4.java
@@ -28,3 +28,42 @@ public class Problem4 {
         System.out.print(answer);
     }
 }
+
+/*
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long M = sc.nextInt();
+        int[] inputs = new int[N];
+        for (int i = 0; i < N; i++) {
+            inputs[i] = sc.nextInt();
+        }
+
+        int lt = 0;
+        int rt = 0;
+        int count = 0;
+        long sum = inputs[0];
+
+        while (true) {
+            if (sum < M && rt != N - 1) {
+                rt++;
+                sum += inputs[rt];
+            }
+            else if (sum < M && rt == N - 1) {
+                break;
+            }
+
+            else if (sum > M) {
+                sum -= inputs[lt];
+                lt++;
+            }
+            else if (sum == M) {
+                count++;
+                sum -= inputs[lt];
+                lt++;
+            }
+        }
+
+        System.out.println(count);
+    }
+ */
