ee967817fc22d5280e9b0a36a48f18565f879eae
201912025 <michi2012@naver.com>
2025-09-02 14:59:12 +0900

    chapter11: Problem2

---
+++ src/main/java/org/problems/javacodingtest/chapter11/Problem12.java
@@ -0,0 +1,19 @@
+package org.problems.javacodingtest.chapter11;
+
+import java.util.Scanner;
+
+public class Problem12 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] D = new int[N + 2];
+        D[1] = 1;
+        D[2] = 2;
+
+        for (int i = 3; i <= N+1; i++) {
+            D[i] = D[i - 1] + D[i - 2];
+        }
+
+        System.out.println(D[N+1]);
+    }
+}
